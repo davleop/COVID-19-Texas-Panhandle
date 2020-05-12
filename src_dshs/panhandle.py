44363087a06e096d9b565e70f52783cb71aa9a50
David Penn <42362379+davleop@users.noreply.github.com>
2020-05-12 13:45:18 -0500

    Created Texas_old

--- src_dshs/panhandle.py
+++ src_dshs/panhandle.py
@@ -1,2 +1,11 @@
 # panhandle.py
 from sys import platform
+from Find import Find
+
+def main():
+	counties = ['Armstrong', 'Briscoe', 'Carson', 'Castro', 'Childress', 'Collingsworth', 'Dallam',
+				'Deaf Smith', 'Donley', 'Gray', 'Hall', 'Hansford', 'Hartley', 'Hemphill','Hutchinson',
+				'Lipscomb', 'Moore', 'Ochiltree', 'Oldham', 'Palmer', 'Potter', 'Randall', 'Roberts',
+				'Sherman', 'Swisher', 'Wheeler']
+
+	if platform == "win32"
