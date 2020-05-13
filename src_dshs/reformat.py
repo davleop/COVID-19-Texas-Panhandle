7e0fec18dc9f604b92a2dfe02b6c0dfc2e6abdde
David Penn <42362379+davleop@users.noreply.github.com>
2020-05-12 20:26:44 -0500

    Processing of Cases complete

--- src_dshs/reformat.py
+++ src_dshs/reformat.py
@@ -37,28 +37,31 @@ def main():
 		new_cols.append(date[:10])
 	rename_dict = {i:j for i,j in zip(old_cols,new_cols)}
 	active_df.rename(columns=rename_dict, inplace=True)
+	active_df.set_index("date",inplace=True)
 	active_df = active_df.T
 	active_df.to_csv(active_out)
 
 	# Process Case Counts
 	case_df = pd.read_csv(case_path)
-	case_df = case_df.replace(r'Cases  ','', regex=True)
+	old_cols = case_df.columns.tolist()
+	case_df.drop(["Population"], axis=1, inplace=True)
+	new_cols = ["date"]
+	for date in old_cols[1:]:
+		new_cols.append("2020-" + date[7:])
+	rename_dict = {i:j for i,j in zip(old_cols,new_cols)}
+	case_df.rename(columns=rename_dict, inplace=True)
+	case_df.set_index("date",inplace=True)
 	case_df = case_df.T
-	case_df.drop(case_df.index[[0]], inplace=True)
 	case_df.to_csv(case_out, index=False)
 
 	# Process Test Totals
 	test_df = pd.read_csv(test_path)
-	test_df = test_df.replace(r' 00:00:00','', regex=True)
 	test_df = test_df.T
-	test_df.drop(test_df.index[[0]], inplace=True)
 	test_df.to_csv(test_out, index=False)
 
 	# Process Fatality Counts
 	fatality_df = pd.read_csv(fatality_path)
-	fatality_df = fatality_df.replace(r' 00:00:00','', regex=True)
 	fatality_df = fatality_df.T
-	fatality_df.drop(fatality_df.index[[0]], inplace=True)
 	fatality_df.to_csv(fatality_out, index=False)
 
 if __name__ == '__main__':
